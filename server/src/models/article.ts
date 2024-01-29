593c8c7644a603d55d303f037c90a169568fd652
Srija-appies <appiessrija@gmail.com>
2024-01-29 19:07:25 +0530

    update for deployment

--- server/src/models/article.ts
+++ server/src/models/article.ts
@@ -23,40 +23,4 @@ const articleSchema = new Schema({
 
 const Article= mongoose.model("Article", articleSchema);
 
-
-const article1 = new Article({
-  title: "Basic Card",
-  imageUrl: "https://www.kasandbox.org/programming-images/avatars/mr-pants-purple.png",
-  content: "Professional",
-  access: "Basic",
-});
-
-const article2 = new Article({
-  title: "Standard 1",
-  imageUrl: "https://www.kasandbox.org/programming-images/avatars/mr-pants-purple.png",
-  content: "Professional",
-  access: "Standard",
-});
-
-const article3 = new Article({
-  title: "Standard 2",
-  imageUrl: "https://www.kasandbox.org/programming-images/avatars/mr-pants-purple.png",
-  content: "Professional",
-  access: "Standard",
-});
-
-const article4 = new Article({
-  title: "Premium",
-  imageUrl: "https://www.kasandbox.org/programming-images/avatars/mr-pants-purple.png",
-  content: "Professional",
-  access: "Premium",
-});
-
-// Save articles to the database
-article1.save();
-article2.save();
-article3.save();
-article4.save();
-
-
 export default Article
